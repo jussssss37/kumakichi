19f48c6b3ae686b37d1336d25a479610113115bd
jussssss37 <jus0374045@gmail.com>
2025-10-05 16:16:07 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+const basePath = isProd ? '/kumakichi' : ''
+
 const nextConfig = {
   output: 'export',
-  basePath: '/kumakichi',
-  assetPrefix: '/kumakichi',
+  basePath: basePath,
+  assetPrefix: basePath,
   trailingSlash: true,
   images: {
     unoptimized: true
+  },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath
   }
 }
 
